b30b1221ecf6b8313a3193a973e544c8977c6a66
namanh29tb@gmail.com <namanh29tb@gmail.com>
2021-11-02 00:32:19 +0700

    update bai tap tren lop ngay 1/11/2021

--- InformationForm/app/src/main/java/com/example/informationform/MainActivity.java
+++ InformationForm/app/src/main/java/com/example/informationform/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.informationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextName = findViewById(R.id.editText_name);
+                String name = editTextName.getText().toString();
+                EditText editTextId = findViewById(R.id.editText_id);
+                String id = editTextId.getText().toString();
+                EditText editTextDob = findViewById(R.id.editText_dob);
+                String dob = editTextDob.getText().toString();
+                EditText editTextPhone = findViewById(R.id.editText_phone);
+                String phone = editTextPhone.getText().toString();
+                EditText editTextEmail = findViewById(R.id.editText_email);
+                String email = editTextEmail.getText().toString();
+
+                if(name.length() == 0){
+                    Toast toast = Toast.makeText(getBaseContext(), "Tên không được để trống", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                if(id.length() == 0){
+                    Toast toast = Toast.makeText(getBaseContext(), "MSSV không được để trống", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                if(dob.length() == 0){
+                    Toast toast = Toast.makeText(getBaseContext(), "Ngày sinh không được để trống", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                if(phone.length() == 0){
+                    Toast toast = Toast.makeText(getBaseContext(), "Số điện thoại không được để trống", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                if(email.length() == 0){
+                    Toast toast = Toast.makeText(getBaseContext(), "Email không được để trống", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+            }
+        });
     }
 }
